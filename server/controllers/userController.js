af3d3a158ca9f27782676bb8dd3b411fa94dd491
SifatSikder <bsse1221@iit.du.ac.bd>
2023-02-12 01:29:47 +0600

    ok

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -7,43 +7,57 @@ const pool = mysql.createPool({
   database: process.env.DB_NAME,
 }); // a cache of database connection so that we can reuse connections when future requests come in
 
-
-
-
 exports.home = (req, res) => {
-    res.render("home");
+  res.render("home");
 };
 
 exports.view_register = (req, res) => {
-    console.log(req.url);
-    res.render("register",{layout: false});
-}; 
-
-exports.register2 = (req, res) => {
-    console.log(req.url);
-    res.render("register2",{layout: false});
-}; 
-
-exports.register3 = (req, res) => {
-    console.log(req.url);
-    res.render("register3",{layout: false});
-}; 
-
-exports.register4 = (req, res) => {
-    console.log(req.url);
-    res.render("register4",{layout: false});
+  console.log(req.url);
+  res.render("register", { layout: false });
 };
 
-exports.register = (req,res)=>{
-    pool.getConnection((err, connection) => {
-        if (err) throw err;
-        const data = req.body;
+exports.register2 = (req, res) => {
+  console.log(req.url);
+  res.render("register2", { layout: false });
+};
 
-        connection.query("INSERT INTO User SET Name = ?, InstitutionalEmail = ?, UniversityRegistrationID = ?, Password = ?,PhoneNumber = ?,bKashNumber = ?",[data.Username,data.email,data.UniversityRegistrationID,data.password,data.PhoneNumber,data.bKashNumber],
-        (err,rows)=>{
-            connection.release();
-            if(!err) res.send("success");
-            else     res.send(err);
-        })
-    })//connecting to the database
+exports.register3 = (req, res) => {
+  console.log(req.url);
+  res.render("register3", { layout: false });
+};
+
+exports.register4 = (req, res) => {
+  console.log(req.url);
+  res.render("register4", { layout: false });
+};
+
+exports.register = (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) throw err;
+    const data = req.body;
+
+    connection.query(
+      "select count(*) as count from User where InstitutionalEmail = ?",
+      [data.email],
+      (err, rows) => {
+        console.log(rows[0].count);
+        if (rows[0].count<1) 
+        {
+            connection.query("INSERT INTO User SET Name = ?, InstitutionalEmail = ?, UniversityRegistrationID = ?, Password = ?,PhoneNumber = ?,bKashNumber = ?",[data.Username,data.email,data.UniversityRegistrationID,data.password,data.PhoneNumber,data.bKashNumber],
+            (err,rows)=>{
+                connection.release();
+                if(!err) res.send("success");
+                else     res.send(err);
+            })
+        }
+        else
+        {
+            console.log("code is here");
+            console.log(req.url);
+            const params = {count: rows[0].count ,message : "User already exists" };
+            res.redirect('/register')  
+        }
+      }
+    );
+  }); //connecting to the database
 };
